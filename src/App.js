16dee1cf1ced9ada356b533241d858fd7942c8be
CrioUser <crio-user@criodo.com>
2023-02-16 11:04:46 +0530

    “ME_QKART_FRONTEND_V2_MODULE_DEPLOYMENT”

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Products from "./components/Products";
 import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkartbackend-kbqc.onrender.com/api/v1`,
 };
 
 function App() {
