d72825af1ca002f3f7f1b275f574eb2d3b5e3790
Khaliq Ansari <khaliqansari771@gmail.com>
2023-12-27 20:22:56 +0530

    Update App.js
    updated the backend endpoint url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Products from "./components/Products";
 import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkart-frontend-projects-backend.onrender.com/api/v1`,
 };
 
 function App() {
